4a0d7e884a2651cf02a1ccf3e69224fe307dd838
SirCipher <tklapwijk92@gmail.com>
2019-11-25 12:19:52 +0000

    Azure continue tests on failure

---
+++ swim-core-java/swim.io.warp/src/test/java/swim/io/warp/Testy.java
@@ -0,0 +1,13 @@
+package swim.io.warp;
+
+import org.testng.annotations.Test;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Testy {
+
+  public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+
+  }
+
+}
