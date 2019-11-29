f1f459f40d628d1f81598a7f933f83255d52e213
SirCipher <tklapwijk92@gmail.com>
2019-11-29 14:50:25 +0000

    Fixes typo

--- swim-core-java/swim.io/src/test/java/swim/io/IpModemBehaviors.java
+++ swim-core-java/swim.io/src/test/java/swim/io/IpModemBehaviors.java
@@ -288,7 +288,7 @@ public abstract class IpModemBehaviors {
       stage.stop();
     }
   }
-  @Test(
+  @Test
   public void testTransmitMultipleLines() {
     final Theater stage = new Theater();
     final IpEndpoint endpoint = new IpEndpoint(stage);
